a504692ed84f1e31bcf43e01e29298bb8812a045
contaqfreefire-alt <contaqfreefire@gmail.com>
2026-09-23 21:20:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import os
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse  # <--- Nova biblioteca importada
 from google import genai
 
 app = FastAPI()
 
-# O Render vai ler a chave configurada no painel de controle deles de forma segura
 API_KEY = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=API_KEY)
 
-@app.get("/")
+# Mudamos a rota inicial para ler e exibir o nosso arquivo index.html
+@app.get("/", response_class=HTMLResponse)
 def raiz():
-    return {"status": "Servidor da IA online no Render!"}
+    if os.path.exists("index.html"):
+        with open("index.html", "r", encoding="utf-8") as arquivo:
+            return arquivo.read()
+    return "<h1>Servidor online, mas arquivo index.html não foi encontrado.</h1>"
 
 @app.get("/perguntar")
 def perguntar_ia(mensagem: str):
     try:
         response = client.models.generate_content(
-           model='gemini-3.6-flash',
-
+            model='gemini-3.6-flash',
             contents=mensagem,
         )
         return {"resposta": response.text}
