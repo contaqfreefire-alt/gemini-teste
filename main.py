09889be9469a9c58376f93b0904c6b344b84e350
contaqfreefire-alt <contaqfreefire@gmail.com>
2026-09-23 21:18:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ def raiz():
 def perguntar_ia(mensagem: str):
     try:
         response = client.models.generate_content(
-            model='gemini-3.6-flash',  # <--- Mudamos apenas este número aqui!
+           model='gemini-3.6-flash',
+
             contents=mensagem,
         )
         return {"resposta": response.text}
